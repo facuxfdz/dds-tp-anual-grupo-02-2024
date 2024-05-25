2a713caea1912748e208787d6ee136cd7aa2f903
Marcos Pedaci <mpedaci@frba.utn.edu.ar>
2024-05-25 20:38:19 -0300

    merga

---
+++ AccesoAlimentario.Core/Entities/DocumentosIdentidad/TipoDocumento.cs
@@ -0,0 +1,9 @@
+namespace AccesoAlimentario.Core.Entities.DocumentosIdentidad;
+
+public enum TipoDocumento
+{
+    DNI,
+    LE,
+    LC,
+    CUIT,
+}
---
+++ AccesoAlimentario.Core/Entities/MediosContacto/Whatsapp.cs
@@ -0,0 +1,11 @@
+namespace AccesoAlimentario.Core.Entities.MediosContacto;
+
+public class Whatsapp : IMedioContacto
+{
+    private string _numero;
+
+    public void Contactar()
+    {
+        throw new NotImplementedException();
+    }
+}
