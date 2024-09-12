a98bb5291565d3e2cbe2568261fae9550f9b39fc
napetruzzi <81927757+napetruzzi@users.noreply.github.com>
2024-09-11 23:37:53 -0300

    "Agrego" TestAccesosAutorizaciones.cs
    Es un mock, hasta que conectemos la DB

--- AccesoAlimentario.Testing/TestAccesosAutorizaciones.cs
+++ AccesoAlimentario.Testing/TestAccesosAutorizaciones.cs
@@ -1,6 +1,39 @@
+using AccesoAlimentario.Core.Entities.Autorizaciones;
+using AccesoAlimentario.Core.Entities.Heladeras;
+using AccesoAlimentario.Core.Entities.Tarjetas;
+using AccesoAlimentario.Core.Servicios;
+
 namespace AccesoAlimentario.Testing;
 
-public class TestAccesosAutorizaciones
+[TestFixture]
+public class TestAccesosAutorizaciones //Para que funcione la BD debe estar conectada, asi validamos que las tarjetas exsitan al menos y que las tarjetas de consumo
 {
+    private Heladera _heladera;
+    private TarjetaColaboracion _tarjetaColaboracionValida;
+    private TarjetaColaboracion _tarjetaColaboracionInvalida;
+
+    [SetUp]
+    public void Setup()
+    {
+        _heladera = new Heladera();
+        _tarjetaColaboracionValida = new TarjetaColaboracion();
+        _tarjetaColaboracionInvalida = new TarjetaColaboracion();
+    }
     
+    [Test]
+    public void TestAccederHeladeraConTarjetaColabValida()
+    {
+        var autorizacion = new AccesoHeladera(_tarjetaColaboracionValida, _heladera, TipoAcceso.RetiroVianda);
+
+        Assert.That(autorizacion.VerificarValidez(), Is.True, "La tarjeta válida debería permitir el acceso.");
+    }
+    
+ /*   [Test]
+    public void TestAccederHeladeraConTarjetaColabInValida()
+    {
+        var autorizacion = new AccesoHeladera(_tarjetaColaboracionInvalida, _heladera, TipoAcceso.RetiroVianda);
+
+        Assert.That(autorizacion.VerificarValidez(), Is.False, "La tarjeta inválida no debería permitir el acceso.");
+    }
+    */
 }
