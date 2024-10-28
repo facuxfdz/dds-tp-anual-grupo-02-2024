03999f455c49ccf95f3f88a0e6097301b25aea37
Franco Piccininno <fpiccininno@frba.utn.edu.ar>
2024-10-27 23:59:07 -0300

    Add new UCs

--- AccesoAlimentario.Operations/Heladeras/ModificacionHeladera.cs
+++ AccesoAlimentario.Operations/Heladeras/ModificacionHeladera.cs
@@ -1,6 +1,80 @@
-﻿namespace AccesoAlimentario.Operations.Heladeras;
+﻿using AccesoAlimentario.Core.DAL;
+using AccesoAlimentario.Core.Entities.Heladeras;
+using AccesoAlimentario.Operations.Dto.Requests.Heladeras;
+using AutoMapper;
+using FluentValidation;
+using MediatR;
+using Microsoft.AspNetCore.Http;
 
-public class ModificacionHeladera
+namespace AccesoAlimentario.Operations.Heladeras;
+
+public static class ModificacionHeladera
 {
+    public class ModificacionHeladeraCommand : IRequest<IResult>
+    {
+        public Guid Id { get; set; } = Guid.Empty;
+        public PuntoEstrategicoRequest PuntoEstrategico { get; set; } = null!;
+        public EstadoHeladera Estado { get; set; } = EstadoHeladera.FueraServicio;
+        public float TemperaturaMinimaConfig { get; set; } = 0;
+        public float TemperaturaMaximaConfig { get; set; } = 0;
+        public List<SensorRequest> Sensores { get; set; } = [];
+    }
     
+    public class ModificacionHeladeraValidator : AbstractValidator<ModificacionHeladeraCommand>
+    {
+        public ModificacionHeladeraValidator()
+        {
+            RuleFor(x => x.PuntoEstrategico)
+                .NotNull();
+            RuleFor(x => x.Estado)
+                .NotNull();
+            RuleFor(x => x.TemperaturaMinimaConfig)
+                .NotNull();
+            RuleFor(x => x.TemperaturaMaximaConfig)
+                .NotNull();
+            RuleFor(x => x.Sensores)
+                .NotNull();
+        }
+    }
+    
+    public class Handler : IRequestHandler<ModificacionHeladeraCommand, IResult>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public Handler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public bool IsDataValid(Heladera h)
+        {
+            if (h.TemperaturaMinimaConfig < h.Modelo.TemperaturaMinima ||
+                h.TemperaturaMaximaConfig > h.Modelo.TemperaturaMaxima)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public async Task<IResult> Handle(ModificacionHeladeraCommand request, CancellationToken cancellationToken)
+        {
+            var heladera = await _unitOfWork.HeladeraRepository.GetByIdAsync(request.Id);
+            if (heladera == null)
+            {
+                return Results.NotFound("La heladera no existe");
+            }
+            
+            if (!IsDataValid(heladera))
+            {
+                return Results.BadRequest("Parametros fuera de los valores permitidos");
+            }
+            
+            await _unitOfWork.HeladeraRepository.UpdateAsync(heladera);
+            await _unitOfWork.SaveChangesAsync();
+            
+            return Results.Ok();
+        }
+    }
 }
--- AccesoAlimentario.Operations/Roles/Tecnicos/ModificacionTecnico.cs
+++ AccesoAlimentario.Operations/Roles/Tecnicos/ModificacionTecnico.cs
@@ -1,6 +1,87 @@
-﻿namespace AccesoAlimentario.Operations.Roles.Tecnicos;
+﻿using AccesoAlimentario.Core.DAL;
+using AccesoAlimentario.Core.Entities.Personas;
+using AccesoAlimentario.Core.Entities.MediosContacto;
+using AccesoAlimentario.Core.Entities.Roles;
+using AccesoAlimentario.Operations.Dto.Requests.MediosDeComunicacion;
+using AccesoAlimentario.Operations.Dto.Requests.Personas;
+using AutoMapper;
+using FluentValidation;
+using MediatR;
+using Microsoft.AspNetCore.Http;
 
-public class ModificacionTecnico
+namespace AccesoAlimentario.Operations.Roles.Tecnicos;
+
+public static class ModificacionTecnico
 {
+    public class ModificacionTecnicoCommand : IRequest<IResult>
+    {
+        public Guid Id { get; set; } = Guid.Empty;
+        public PersonaRequest Persona { get; set; } = null!;
+        public List<MedioDeContactoRequest> MediosDeContacto = [];
+        public float AreaCoberturaLatitud { get; set; } = 0;
+        public float AreaCoberturaLongitud { get; set; } = 0;
+        public float AreaCoberturaRadio { get; set; } = 0;
+    }
     
+    public class ModificacionTecnicoValidator : AbstractValidator<ModificacionTecnicoCommand>
+    {
+        public ModificacionTecnicoValidator()
+        {
+            RuleFor(x => x.Persona)
+                .NotNull();
+            RuleFor(x => x.MediosDeContacto)
+                .NotNull();
+            RuleFor(x => x.AreaCoberturaLatitud)
+                .NotNull();
+            RuleFor(x => x.AreaCoberturaLongitud)
+                .NotNull();
+            RuleFor(x => x.AreaCoberturaRadio)
+                .NotNull();
+        }
+    }
+    
+    public class Handler : IRequestHandler<ModificacionTecnicoCommand, IResult>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public Handler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<IResult> Handle(ModificacionTecnicoCommand request, CancellationToken cancellationToken)
+        {
+            var validator = new ModificacionTecnicoValidator();
+            var validationResult = await validator.ValidateAsync(request, cancellationToken);
+            if (!validationResult.IsValid)
+            {
+                return Results.Problem();
+            }
+            
+            var persona = _mapper.Map<Persona>(request.Persona);
+            var mediosDeContacto = _mapper.Map<List<MedioContacto>>(request.MediosDeContacto);
+            var areaCobertura = new AreaCobertura(request.AreaCoberturaLatitud, request.AreaCoberturaLongitud, request.AreaCoberturaRadio);
+            
+            var tecnico = await _unitOfWork.TecnicoRepository.GetByIdAsync(request.Id);
+            if (tecnico == null)
+            {
+                return Results.Problem("No se encontró el técnico");
+            }
+            
+            persona.MediosDeContacto = mediosDeContacto;
+            tecnico.Persona = persona;
+            tecnico.AreaCobertura = areaCobertura;
+            
+            await _unitOfWork.PersonaRepository.UpdateAsync(tecnico.Persona);
+            await _unitOfWork.AreaCoberturaRepository.UpdateAsync(tecnico.AreaCobertura);
+            await _unitOfWork.TecnicoRepository.UpdateAsync(tecnico);
+            await _unitOfWork.SaveChangesAsync();
+            
+            
+            await _unitOfWork.SaveChangesAsync();
+            return Results.Ok();
+        }
+    }
 }
