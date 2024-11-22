0921ecdf5b1ac51b22a124e6de61c3951f0bf066
Facu <facuassain@gmail.com>
2024-11-22 00:32:53 -0300

    Task def for front

--- deployment/ecs/main.tf
+++ deployment/ecs/main.tf
@@ -57,8 +57,8 @@ module "ecs" {
   }
 
   services = {
-    initial_svc = {
-      name                           = var.service_name
+    front_svc = {
+      name                           = "${var.service_name}-front"
       tasks_iam_role_use_name_prefix = false
       ignore_task_definition_changes = true
       cpu                            = 512
@@ -70,9 +70,55 @@ module "ecs" {
       load_balancer = {
         service = {
           target_group_arn = data.aws_lb_target_group.alb_tg.arn
-          container_name   = var.service_name
+          container_name   = "${var.service_name}-front"
           container_port   = 8085
         }
+      }
+      security_group_rules = {
+        alb_ingress = {
+          type        = "ingress"
+          from_port   = 8085
+          to_port     = 8085
+          protocol    = "tcp"
+          description = "Service port"
+          cidr_blocks = ["0.0.0.0/0"]
+        }
+        egress_all = {
+          type        = "egress"
+          from_port   = 0
+          to_port     = 0
+          protocol    = "-1"
+          cidr_blocks = ["0.0.0.0/0"]
+        }
+      }
+      container_definitions = {
+        ecs-sample = {
+          name      = var.service_name
+          cpu       = 512
+          memory    = 1024
+          essential = true
+          image     = "public.ecr.aws/aws-containers/ecsdemo-frontend:776fd50"
+          port_mappings = [
+            {
+              name          = "ecs-sample"
+              containerPort = 8085
+              hostPort      = 8085
+              protocol      = "tcp"
+            }
+          ]
+        }
+      }
+    }
+    
+    back_svc = {
+      name                           = var.service_name
+      tasks_iam_role_use_name_prefix = false
+      ignore_task_definition_changes = true
+      cpu                            = 512
+      memory                         = 1024
+      subnet_ids                     = data.aws_subnets.private.ids
+      tasks_iam_role_policies = {
+        secrets_manager = aws_iam_policy.secrets_manager.arn
       }
       security_group_rules = {
         alb_ingress = {
