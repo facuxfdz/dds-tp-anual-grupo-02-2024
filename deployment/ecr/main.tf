c408afeaf4f38e2b526de4fad19eedc7cb62ec36
Facu <facuassain@gmail.com>
2024-11-22 01:50:54 -0300

    Create ecr front

--- deployment/ecr/main.tf
+++ deployment/ecr/main.tf
@@ -10,3 +10,16 @@ module "ecr" {
     Terraform = "true"
   }
 }
+
+module "ecr_front" {
+    source  = "terraform-aws-modules/ecr/aws"
+    version = "2.3.0"
+    
+    repository_name = "${var.repository_name}/front"
+    
+    repository_lifecycle_policy = templatefile(var.lifecycle_policy_file, {})
+    
+    tags = {
+        Terraform = "true"
+    }
+}
