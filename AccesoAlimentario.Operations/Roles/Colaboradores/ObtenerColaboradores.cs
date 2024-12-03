096577d476115345285a4b3c450cafd3516b3204
Facu <facuassain@gmail.com>
2024-12-03 14:35:28 -0300

    Colaboradores

---
+++ AccesoAlimentario.Operations/Roles/Colaboradores/ObtenerColaboradores.cs
@@ -0,0 +1,35 @@
+﻿using AccesoAlimentario.Core.DAL;
+using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace AccesoAlimentario.Operations.Roles.Colaboradores;
+
+public class ObtenerColaboradores
+{
+    public class ObtenerAllCommand : IRequest<IResult>
+    {
+    }
+    public class ObtenerAllHandler : IRequestHandler<ObtenerAllCommand, IResult>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        private readonly ILogger _logger;
+        
+        public ObtenerAllHandler(IUnitOfWork unitOfWork, IMapper mapper, ILogger<ObtenerAllHandler> logger)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+            _logger = logger;
+        }
+        
+        public async Task<IResult> Handle(ObtenerAllCommand request, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Obteniendo colaboradores");
+            var query = _unitOfWork.ColaboradorRepository.GetQueryable();
+            var colaboradores = await _unitOfWork.ColaboradorRepository.GetCollectionAsync(query);
+            return Results.Ok(colaboradores);
+        }
+    }
+}
