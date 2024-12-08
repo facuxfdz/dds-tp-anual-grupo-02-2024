f6373c5d86c2441738b44dc899b41c54b49bb2f9
Facu <facuassain@gmail.com>
2024-12-08 14:40:01 -0300

    fmt

--- deployment/ecs/main.tf
+++ deployment/ecs/main.tf
@@ -77,8 +77,8 @@ resource "aws_vpc_security_group_ingress_rule" "task_sg_ingress" {
 
 resource "aws_vpc_security_group_egress_rule" "task_sg_egress" {
   security_group_id = aws_security_group.task_sg.id
-  cidr_ipv4 = "0.0.0.0/0"
-  ip_protocol = "-1"
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
 }
 
 module "ecs" {
